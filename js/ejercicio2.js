5d34347fa77dccf48a051d98ce147f2155c8d750
adrianag57 <adrianag57@educastur.es>
2023-10-02 13:28:36 +0200

    Ejercicio 2

---
+++ js/ejercicio2.js
@@ -0,0 +1,12 @@
+var x = prompt("Introduce un número(puede ser decimal");
+
+var y = prompt("Introduce otro número(puede ser decimal");
+
+Math.ceil(y);
+Math.floor(x);
+Math.round(y);
+Math.random();
+Math.pow(x, y);
+Math.sqrt(y);
+Math.max(x, y);
+Math.min(x, y);
