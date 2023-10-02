3e78274ce0cf42a15ec26ab8f05b21294bde26f7
adrianag57 <adrianag57@educastur.es>
2023-10-02 13:56:14 +0200

    cambios

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,12 +1,10 @@
-var x = prompt("Introduce un número(puede ser decimal");
+console.log(typeof(x));
+console.log(typeof(y));
 
-var y = prompt("Introduce otro número(puede ser decimal");
-
-Math.ceil(y);
-Math.floor(x);
-Math.round(y);
-Math.random();
-Math.pow(x, y);
-Math.sqrt(y);
-Math.max(x, y);
-Math.min(x, y);
+console.log("El número " + y + "redondeado al alza es " + Math.ceil(y));
+console.log("El número " + x + "redondeado a la baja es " + Math.ceil(y));
+console.log("El entero más próximo de " + y + " es " + Math.round);
+console.log("Un valor aleatorio entre 0 y 1 es:" + Math.random);
+console.log("El valor de " + x + " elevado a " + y + " es " + Math.pow(x,y));
+console.log("La raiz cudrada de " + y + " es " + Math.sqrt(y));
+console.log("El máximo entre " + x + " y " + y + " es " + Math.max(x, y) + "y el mínimo es " + Math.min(x, y));
