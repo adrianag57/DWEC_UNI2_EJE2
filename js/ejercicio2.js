28ba82990d63cc298f8f6255591e090ad8224b19
adrianag57 <adrianag57@educastur.es>
2023-10-18 16:50:48 +0200

    cambios

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,10 +1,10 @@
-console.log(typeof(x));
-console.log(typeof(y));
+var x = parseFloat(prompt("Introduzca un valor numérico:"));
+var y = parseFloat(prompt("Introduzca el segundo valor numérico:"));
 
-console.log("El número " + y + "redondeado al alza es " + Math.ceil(y));
-console.log("El número " + x + "redondeado a la baja es " + Math.ceil(y));
-console.log("El entero más próximo de " + y + " es " + Math.round);
-console.log("Un valor aleatorio entre 0 y 1 es:" + Math.random);
+console.log("El número " + y + " redondeado al alza es " + Math.ceil(y));
+console.log("El número " + x + " redondeado a la baja es " + Math.ceil(y));
+console.log("El entero más próximo de " + y + " es " + Math.round(y));
+console.log("Un valor aleatorio entre 0 y 1 es: " + Math.random());
 console.log("El valor de " + x + " elevado a " + y + " es " + Math.pow(x,y));
 console.log("La raiz cudrada de " + y + " es " + Math.sqrt(y));
-console.log("El máximo entre " + x + " y " + y + " es " + Math.max(x, y) + "y el mínimo es " + Math.min(x, y));
+console.log("El máximo entre " + x + " y " + y + " es " + Math.max(x, y) + " y el mínimo es " + Math.min(x, y));
